f496b90cafdfb3fdd035ab8867b7c814fb15679f
Revati Sonar <rsonar@shulware.com>
2025-11-24 17:05:37 +0530

    fix(rethink-bh-mobile): fixed code for JIRA-RBH-9

--- packages/rethink-bh-mobile/pages/mobilepages/data.page.ts
+++ packages/rethink-bh-mobile/pages/mobilepages/data.page.ts
@@ -3,9 +3,7 @@ import { MobileBasePage } from '../../../../globals/mobile/mobile-base.page';
 
 class DataPage extends MobileBasePage {
   get dataButton() {
-    return driver.isAndroid
-      ? $('(//androidx.compose.ui.platform.ComposeView//android.view.View[2]//android.view.View[2])[4]').getElement()
-      : $('#iOSLocator').getElement(); // locator for Android
+    return driver.isAndroid ? $('//android.widget.TextView[@text="Data"]').getElement() : $('#iOSLocator').getElement(); // locator for Android
   }
 
   get dataTitle() {
@@ -33,19 +31,6 @@ class DataPage extends MobileBasePage {
       ? $('//android.view.View[@content-desc="Bug Bun, Wednesday, Nov 19, 5:48pm"]').getElement()
       : $('#iOSLocator').getElement();
     // locator for Android
-  }
-
-  async clickOnDataButton() {
-    await super.click(await this.dataButton);
-  }
-  async clickOnPlusIcon() {
-    await super.click(await this.plusIcon);
-  }
-  async clickOnPencilIcon() {
-    await super.click(await this.editIcon);
-  }
-  async clickOnIncludeButton() {
-    await super.click(await this.includeButton);
   }
 }
 export const samplePage = new DataPage();
--- packages/rethink-bh-mobile/pages/mobilepages/selectClients.page.ts
+++ packages/rethink-bh-mobile/pages/mobilepages/selectClients.page.ts
@@ -1,5 +1,4 @@
 /* eslint-disable class-methods-use-this */
-import { MilliSeconds } from 'globals/enums/milliseconds.enum';
 import { MobileBasePage } from '../../../../globals/mobile/mobile-base.page';
 
 class SelectClientsPage extends MobileBasePage {
@@ -9,7 +8,7 @@ class SelectClientsPage extends MobileBasePage {
       : $('#iOSLocator').getElement(); // locator for Android
   }
 
-  get searchInput() {
+  get searchClient() {
     return driver.isAndroid
       ? $('//android.widget.EditText[@content-desc="Client Search"]').getElement()
       : $('#iOSLocator').getElement(); // locator for Android
@@ -17,19 +16,19 @@ class SelectClientsPage extends MobileBasePage {
 
   get clearButton() {
     return driver.isAndroid
-      ? $('(//androidx.compose.ui.platform.ComposeView//android.widget.Button)[1]').getElement()
+      ? $('//android.widget.TextView[@text="Clear"]').getElement()
       : $('#iOSLocator').getElement(); // locator for Android
   }
 
-  get backButton() {
+  get back() {
     return driver.isAndroid
       ? $('//android.view.View[@content-desc="Back"]').getElement()
       : $('#iOSLocator').getElement(); // locator for Android
   }
 
-  get selectButton() {
+  get select() {
     return driver.isAndroid
-      ? $('//androidx.compose.ui.platform.ComposeView//android.view.View[2]/android.widget.Button').getElement()
+      ? $('	//android.widget.TextView[@text="Select"]').getElement()
       : $('#iOSLocator').getElement(); // locator for Android
   }
 
@@ -39,31 +38,10 @@ class SelectClientsPage extends MobileBasePage {
       : $('#iOSLocator').getElement(); // locator for Android
   }
 
-  get checkbox() {
+  get selectProgramsCheckbox() {
     return driver.isAndroid
       ? $('//android.widget.CheckBox[@content-desc="!!!, !!!"]').getElement()
       : $('#iOSLocator').getElement(); // locator for Android
-  }
-  //   const items = await $$('//android.widget.ImageView[starts-with(@content-desc, "Preview Program")]');
-  //   for (const item of items) {
-  //   const desc = await item.getAttribute('content-desc');
-  //   console.log(`Found: ${desc}`);
-  //   await item.click(); // or any other action
-  // }
-  async clickOnClearButton() {
-    await super.click(await this.clearButton);
-  }
-  async clickOnEyeIcon() {
-    await super.click(await this.eyeIcon);
-  }
-  async clickOnSelectButton() {
-    await super.click(await this.selectButton);
-  }
-  async clickOnBackButton() {
-    await super.click(await this.backButton);
-  }
-  async clickOnSearchInput() {
-    await super.typeText(await this.searchInput, 'ABC', MilliSeconds.S);
   }
 }
 export const samplePage = new SelectClientsPage();
